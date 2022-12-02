cb529b877162e6a464d26b137bf97424ab90f186
Karim <kizpucci@gmail.com>
2022-12-02 09:48:51 +0100

    parking corretto

--- index.php
+++ index.php
@@ -3,45 +3,39 @@
         [
             'name' => 'Hotel Belvedere',
             'description' => 'Hotel Belvedere Descrizione',
-            'parking' => 'true',
+            'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4
         ],
         [
             'name' => 'Hotel Futuro',
             'description' => 'Hotel Futuro Descrizione',
-            'parking' => 'true',
+            'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2
         ],
         [
             'name' => 'Hotel Rivamare',
             'description' => 'Hotel Rivamare Descrizione',
-            'parking' => 'false',
+            'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1
         ],
         [
             'name' => 'Hotel Bellavista',
             'description' => 'Hotel Bellavista Descrizione',
-            'parking' => 'false',
+            'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5
         ],
         [
             'name' => 'Hotel Milano',
             'description' => 'Hotel Milano Descrizione',
-            'parking' => 'true',
+            'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50
         ],
-    ];
-
-    <?php foreach ($hotel as $key){
-        if($key === 'parking'){
-            echo "<td>SI</td>"
-        }
-    }
+    ]
 ?>
 
 
@@ -76,7 +70,7 @@
                         <?php echo $hotel['description']?>
                     </td>
                     <td>
-                        <?php echo $hotel['parking']? 'SI' : 'NO'?>
+                        <?php echo $hotel['parking'] ? 'SI' : 'NO'?>
                     </td>
                     <td>
                         <?php echo $hotel['vote']?>
